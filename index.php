d09c0dab3babd5080bd62903d5915818a2143611
Manuel Razzari <manuel@cvam.com.ar>
2012-11-02 17:51:22 -0300

    Commit inicial, importando HTML de @sbustelo.

---
+++ index.php
@@ -0,0 +1,350 @@
+<html lang="es">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+	
+	<title>UX2012: Jornada de Experiencia de Usuario</title> 
+	
+	<meta content="" name="keywords">
+
+	<meta content="UX2012: Jornada de Experiencia de Usuario" property="og:title">
+	<meta content="http://www.ux2012.com.ar" property="og:url">
+	<meta content="UX2012: 28/nov/2012 Centro Cultural San Martín, en el marco del Día Mundial de la Usabilidad. El principal evento profesional en Argentina sobre Experiencia de Usuario, Usabilidad, Diseño de Interacción, Accesibilidad y temáticas relacionadas." property="og:description">
+
+	<link href="rsrc/base.css" media="screen" rel="stylesheet" type="text/css">	
+</head>
+
+
+<body>
+
+<div class="mainhead" id="">
+	<div class="mbody">
+		<ul id="nav1">
+			<li>
+				<a href="./#top">Home</a>
+			</li>
+			<li>
+				<a href="./#programa">Programa</a>
+			</li>
+			<li>
+				<a href="https://eventioz.com/ux2012/registrations/new">Registrarse ahora</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<div class="mainhead2" id="top">
+	<div class="mbody">
+	
+		<div class="layoutContentEtc">
+			<div class="colContent">
+				<h1>
+				<span style="margin-left:-6pt;">UX</span><span class="accent">2<span style="letter-spacing:-.05em;">0<span style="letter-spacing:-.1em;">12</span></span></span>
+				<?php /*
+					<br />
+					<img src="img/ux2012-h1.gif" alt="" width="495" height="105" alt="UX2012"/>
+				*/ ?>
+				</h1>
+				
+				<p style="font-size:24pt; margin:-16pt 0 6pt;"><strong>Experiencia de Usuario</strong></p>
+				<p style="font-size:18pt;"><strong>Usabilidad, Accesibilidad, Diseño y Tecnología</strong></p>
+				<p style="font-size:16pt;">En el marco del Día Mundial de la Usabilidad.</p>
+				<p style="font-size:18pt; margin:20pt 0 0;"><strong class="accent">28 de noviembre 2012, 9 a 19 hs.</strong></p>
+
+				
+			</div><!-- /colContent -->
+
+			<div class="colEtc">
+				<div style="position:absolute; bottom:6pt; right:0; width:180px; height:180px;">
+				<a href="https://maps.google.com/maps?q=Sarmiento+1551,+Buenos+Aires,+Argentina" target="_blank"><img alt="Mapa" src="img/staticmap.png"  height="178" width="178" style="border:1px solid #ccc;" /></a> 
+				</div>
+
+			</div><!-- /colEtc -->
+
+		</div><!-- /layoutContentEtc -->
+
+		
+	</div>
+
+
+</div>
+
+
+<div class="mainhead3" id="top">
+	<div class="mbody">
+		<strong>Centro Cultural San Martín</strong> • Sarmiento 1551, Buenos Aires
+	</div>
+</div>
+
+
+<div class="mainbody">
+	<div class="mbody">
+	
+		<div class="layoutContentEtc">
+			<div class="colContent">
+
+
+				<p style="margin-top:16pt;">
+					UX2012 es el principal evento profesional en Argentina sobre Experiencia de Usuario, Usabilidad, Diseño de Interacción, Accesibilidad y temáticas relacionadas.
+				</p>
+				<p>
+					La jornada se realizará el día 28 de noviembre de 2012 de 9 a 19 hs. en el Centro Cultural General San Martín de la Ciudad de Buenos Aires, en el marco del Día Mundial de la Usabilidad. Estará orientada a profesionales y estudiantes de diseño, comunicación y desarrollo, que busquen generar una buena experiencia de usuario en sus productos digitales.
+				</p>
+				
+				<p>
+					<strong class="accent">
+					El evento es gratuito, requiere registro previo para asegurar lugar.
+					</strong>
+				</p>
+				<p>
+					<span class="button big"><a href="https://eventioz.com/ux2012/registrations/new">Registrate ahora &raquo;</a></span>
+					<br />&nbsp;
+				</p>
+
+
+				<h4>
+					Contará con:
+				</h4>
+				<ul>
+					<li>
+						<strong>Charlas introductorias</strong>, que brindarán las herramientas y metodologías básicas para incorporar la usabilidad y la accesibilidad en los procesos de diseño y desarrollo de interfaces para productos digitales interactivos.
+					</li>
+					<li>
+						<strong>Charlas avanzadas</strong>, dirigidas a profesionales que quieren perfeccionarse en las técnicas y metodologías que ya utilizan para mejorar la experiencia de sus usuarios.
+					</li>
+					<li>
+						<strong>Talleres</strong> para poner en práctica las técnicas básicas del Diseño Centrado en el Usuario.
+					</li>
+				</ul>
+				
+				
+
+<h2 id="programa">Programa</h2>
+
+<table class="programa">
+
+<thead>
+	<tr>
+		<th>
+		</th>
+		<th>Sala C</th>
+		<th>Sala D</th>
+		<th>Aula 1</th>
+		<th>Laboratorio</th>
+	</tr>
+</thead>
+
+<tbody>
+	<tr>
+		<th>9:00</th>
+		<td colspan="4">Acreditación</td>
+	</tr>
+
+	<tr>
+		<th>10:00</th>
+		<td><strong>Qué podemos aprender los diseñadores de las metodologías ágiles</strong> Santiago Bustelo</td>
+		<td><strong>Innovar desde el diseño de Experiencia de Usuario</strong> Ivan Wolcan</td>
+		<td><strong>Neuropsicología aplicada a la Experiencia de Usuario</strong> Diego López Castan</td>
+		<td><strong>Casos y Experiencias de un equipo de UX</strong> Emiliano Horcada</td>
+	</tr>
+
+	<tr>
+		<th>11:00</th>
+		<td colspan="4">Break</td>
+	</tr>
+
+	<tr>
+		<th>11:30</th>
+		<td><strong>Microinteracciones</strong> Rodrigo Galindez</td>
+		<td><strong>Tecnologías para la Salud y la Discapacidad</strong> Rafael Kohanoff</td>
+		<td><strong>Sistemas de producto-servicio, experiencias de valor desde una mirada holística</strong> Yanina Guerzovich</td>
+		<td><strong>Diseñando Storyboards con el usuario a partir de &quot;personas y escenarios&quot;</strong> Fernando Feller</td>
+	</tr>
+
+	<tr>
+		<th>12:30</th>
+		<td colspan="4">Receso para almuerzo</td>
+	</tr>
+
+	<tr>
+		<th>14:00</th>
+		<td><strong>Accesibilidad práctica con HTML5 + CSS3 + ARIA</strong> Manuel Razzari - Camilo Kawerin</td>
+		<td><strong>Usabilidad: momento eureka!</strong> Verónica Traynor</td>
+		<td><strong>Sociología de la Usabilidad: ¿Qué le aporta el diseño de interacción a la sociedad?</strong> Lorena Paz</td>
+		<td rowspan="2"><strong>Taller: Modelado ágil en UX</strong> Eduardo Mercovich</td>
+	</tr>
+
+	<tr>
+		<th>15:00</th>
+		<td colspan="3">Break</td>
+	</tr>
+
+	<tr>
+		<th>15:30</th>
+		<td><strong>Arquitectura de información cross-channel</strong> Gonzalo Auza</td>
+		<td><strong>El diseñador de UX, un especialista con múltiples personalidades</strong> Michael Dorka</td>
+		<td><strong>Periodismo usable</strong> Emiliano Cosenza</td>
+		<td rowspan="2"><strong>Taller: Modelado ágil en UX (repite)</strong> Eduardo Mercovich</td>
+	</tr>
+
+	<tr>
+		<th>16:30</th>
+		<td>Break</td>
+		<td rowspan="2"><strong>Taller de prototipado rápido y testeo de usabilidad con usuarios</strong> Guillermo Ermel</td>
+		<td>Break</td>
+	</tr>
+	
+
+	<tr>
+		<th>17:00</th>
+		<td><strong>Lean UX</strong> Juan Manuel Carraro</td>
+		<td><?php /*  <strong>Usabilidad desde el caso de uso</strong> Víctor Pezzeti */ ?></td>
+	</tr>
+
+	<tr>
+		<th>18:00</th>
+		<td colspan="4">Cierre</td>
+	</tr>
+
+	
+</tbody>
+
+
+</table>
+
+
+
+				<p>
+					<strong class="accent">
+					El evento es gratuito, requiere registro previo para asegurar lugar.
+					</strong>
+				</p>
+				<p>
+					<span class="button big"><a href="https://eventioz.com/ux2012/registrations/new">Registrate ahora &raquo;</a></span>
+					<br />&nbsp;
+				</p>
+
+
+
+			</div><!-- /colContent -->
+
+			<div class="colEtc">
+			
+<ul class="logos">
+	<li><h4>Organizan:</h4>
+		<ul>
+			<li><p><img src="img/csm.gif" alt="" width="148" height="90" /></p></li>
+			<li><p><img src="img/ixda-ba.gif" alt="" width="114" height="47" /></p></li>
+		</ul>
+	</li>
+
+	<li><h4>Auspician:</h4>
+		<ul>
+			<li><p><img src="img/avantrip.gif" alt="" width="121" height="120" /></p></li>
+		</ul>
+	</li>
+	
+	<li><h4>Apoyan:</h4>
+		<ul>
+			<li><p><img src="img/cvam.gif" alt="" width="148" height="72" /></p></li>
+			<li><p><img src="img/keikendo.gif" alt="" width="148" height="36" /></p></li>
+		</ul>
+	</li>
+
+	<li><h4>Convocan:</h4>
+		<ul>
+			<li><p><img src="img/uxpa-ar.gif" alt="" width="148" height="69" /></p></li>
+			<li><p><img src="img/isoc-ar.gif" alt="" width="148" height="37" /></p></li>
+			<li><p><img src="img/mdi.gif" alt="" width="132" height="53" /></p></li>
+		</ul>
+	</li>
+</ul>
+
+
+	
+			</div><!-- /colEtc -->
+
+		</div><!-- /layoutContentEtc -->
+
+
+</div><!-- /mbody -->
+
+</div><!-- /mainbody -->
+				
+
+
+
+
+
+
+
+
+<?php /* 
+				<p>
+					<strong>Conferencistas confirmados:</strong>
+				</p>
+				<ul>
+					<li>
+						<strong>Gonzalo Auza</strong> - Director, Inter-Cultura
+					</li>
+					<li>
+						<strong>Santiago Bustelo</strong> - Director de Diseño, Keikendo
+					</li>
+					<li>
+						<strong>Juan Manuel Carraro</strong> - Dirección de Experiencia de Usuario, Keikendo
+					</li>
+					<li>
+						<strong>Rafael Kohanoff -&nbsp;</strong>Director del Centro de Tecnologías para la Salud y Discapacidad del INTI
+					</li>
+					<li>
+						<strong>Michael Dorka</strong> - Fundador de intuitive+
+					</li>
+					<li>
+						<strong>Guillermo Ermel</strong> - Responsable de User Research de MercadoLibre.com
+					</li>
+					<li>
+						<strong>Rodrigo Galindez</strong> - Diseñador de Producto, Lemon Inc.
+					</li>
+					<li>
+						<strong>Yanina Guerzovich</strong> - Diseñadora de Experiencia de Usuario, Globant
+					</li>
+					<li>
+						<strong>Emiliano Horcada</strong> - User Experience Director at Globant
+					</li>
+					<li>
+						<strong>Camilo Kawerin</strong> - Front-end Developer, Con Vista Al Mar y Radio Nacional Argentina
+					</li>
+					<li>
+						<strong>Diego Lopez Castan</strong> - Analista de Experiencia de Usuario, Personal
+					</li>
+					<li>
+						<strong>Eduardo Mercovich</strong> - Co-fundador y Director en MamaGrande
+					</li>
+					<li>
+						<strong>Emiliano Cosenza</strong> - &nbsp;Periodista. Semi-Senior User Experience Editor en MercadoLibre.com
+					</li>
+					<li>
+						<strong>Lorena Paz</strong> - Socióloga abocada a la Cooperación Tecnológica.
+					</li>
+					<li>
+						<strong>Manuel Razzari</strong> - Diseñador de interacción, Con Vista Al Mar
+					</li>
+					<li>
+						<strong>Verónica Traynor</strong> - Manager de Usabilidad en Avantrip
+					</li>
+					<li>
+						<strong>Ivan Wolcan</strong> - Innovation Lead (Design Strategist/UX Specialist), Globant
+					</li>
+				</ul>
+
+*/ ?>
+
+<div class="mainfoot">
+
+</div><!-- /mainfoot -->
+
+
+</body>
+</html>
