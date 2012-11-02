567163895f3422310203d060d76de1ce8a8c9eb7
Manuel Razzari <manuel@cvam.com.ar>
2012-11-02 19:25:50 -0300

    Cambio de orden en sponsors y apoyan. Links en sidebar.

--- index.php
+++ index.php
@@ -232,35 +232,36 @@
 
 			<div class="colEtc">
 			
-<ul class="logos">
-	<li><h4>Organizan:</h4>
-		<ul>
-			<li><p><img src="img/csm.gif" alt="" width="148" height="90" /></p></li>
-			<li><p><img src="img/ixda-ba.gif" alt="" width="114" height="47" /></p></li>
-		</ul>
-	</li>
+				<ul class="logos">
+					<li><h4>Organizan:</h4>
+						<ul>
+							<li><p><a href="http://www.centroculturalsanmartin.com/"><img src="img/csm.gif" alt="Centro Cultural General San Martín" width="148" height="90" /></a></p></li>
+							<li><p><a href="http://ixda.com.ar/"><img src="img/ixda-ba.gif" alt="IxDA Buenos Aires, Asociación de Diseño de Interacción" width="114" height="47" /></a></p></li>
+						</ul>
+					</li>
 
-	<li><h4>Auspician:</h4>
-		<ul>
-			<li><p><img src="img/avantrip.gif" alt="" width="121" height="120" /></p></li>
-		</ul>
-	</li>
-	
-	<li><h4>Apoyan:</h4>
-		<ul>
-			<li><p><img src="img/cvam.gif" alt="" width="148" height="72" /></p></li>
-			<li><p><img src="img/keikendo.gif" alt="" width="148" height="36" /></p></li>
-		</ul>
-	</li>
+					<li><h4>Auspician:</h4>
+						<ul>
+							<li><p><a href="http://www.avantrip.com/"><img src="img/avantrip.gif" alt="Avantrip" width="121" height="120" /></a></p></li>
+						</ul>
+					</li>
 
-	<li><h4>Convocan:</h4>
-		<ul>
-			<li><p><img src="img/uxpa-ar.gif" alt="" width="148" height="69" /></p></li>
-			<li><p><img src="img/isoc-ar.gif" alt="" width="148" height="37" /></p></li>
-			<li><p><img src="img/mdi.gif" alt="" width="132" height="53" /></p></li>
-		</ul>
-	</li>
-</ul>
+					<li><h4>Convocan:</h4>
+						<ul>
+							<li><p><a href="http://uxpa.org.ar/"><img src="img/uxpa-ar.gif" alt="UXPA Argentina" width="148" height="69" /></a></p></li>
+							<li><p><a href="http://www.isoc.org.ar/"><img src="img/isoc-ar.gif" alt="ISOC Argentina" width="148" height="37" /></a></p></li>
+							<li><p><a href="http://disenoinclusivo.org.ar/"><img src="img/mdi.gif" alt="Movimiento de Diseño Inclusivo" width="132" height="53" /></a></p></li>
+						</ul>
+					</li>
+
+					<li><h4>Apoyan:</h4>
+						<ul>
+							<li><p><a href="http://convistaalmar.com.ar/"><img src="img/cvam.gif" alt="Con Vista Al Mar" width="148" height="72" /></a></p></li>
+							<li><p><a href="http://keikendo.com/"><img src="img/keikendo.gif" alt="Keikendo" width="148" height="36" /></a></p></li>
+						</ul>
+					</li>
+
+				</ul>
 
 
 	
