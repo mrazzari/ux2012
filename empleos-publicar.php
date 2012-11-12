9a474bcade6993c5d144a676827c8e79592b15e8
Santiago Bustelo <santiago@bustelo.com.ar>
2012-11-12 17:09:11 -0300

    Avance de empleos-publicar.php

---
+++ empleos-publicar.php
@@ -0,0 +1,116 @@
+<?php 
+	include("bits/common.php");
+	include("bits/actividades-fns.php");
+	include("bits/actividades-db.php");
+	
+	$pageKind = "single";
+	
+	?><html lang="es">
+
+<?php /*<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+	
+	<title>UX2012: Jornada de Experiencia de Usuario</title> 
+	
+	<meta content="" name="keywords">
+
+	<meta content="UX2012: Jornada de Experiencia de Usuario" property="og:title">
+	<meta content="http://www.ux2012.com.ar" property="og:url">
+	<meta content="UX2012: 28/nov/2012 Centro Cultural San Martín, en el marco del Día Mundial de la Usabilidad. El principal evento profesional en Argentina sobre Experiencia de Usuario, Usabilidad, Diseño de Interacción, Accesibilidad y temáticas relacionadas." property="og:description">
+
+	<link href="rsrc/base.css" media="screen" rel="stylesheet" type="text/css">	
+</head>*/ ?>
+
+<?php
+
+	$pageTitle = "Sumate como sponsor recruiter y public&aacute; tu aviso";
+	include ( "bits/_htmlhead.php" );
+	?>
+	
+<body class="single">
+
+
+<?php include ( "bits/mainheads.php" ); ?>
+
+
+<div class="mainbody" id="content">
+	<div class="mbody">
+	
+		<div class="layoutContentEtc">
+			<div class="colContent">
+
+
+
+<h2>Sumate como sponsor recruiter y public&aacute; tu aviso</h2>
+
+<p>Cum soluta nobis eleifend option congue nihil imperdiet doming id. Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi qui nunc!</p>
+
+<h3>Romulus remusque a fluius ierunt</h3>
+
+<p>Usus legentis in iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod ii?</p>
+
+<ol>
+	<li>Realizás tu pago de alguna de estas maneras:
+		<ul>
+			<li>Tarjeta de crédito o débito: <a href="#">ver instrucciones</a></li>
+			<li>Paypal <a href="#">ver instrucciones</a></li>
+			<li>Transferencia bancaria <a href="#">ver instrucciones</a></li>
+			<li>Personalmente: efectivo o cheque <a href="#">ver instrucciones</a></li>
+		
+		</ul>
+	</li>
+	<li>Nos enviás un mail a sponsors@ux2012.com.ar:
+		<ul>
+			<li>Para pagos online: nombre y apellido del titular de la tarjeta o cuenta bancaria. Para transferencias, adjuntar comprobante.</li>
+			<li>Nombre y apellido de la credencial a personalizar.</li>
+			<li>Nombre de la empresa.</li>
+			<li>Foto avatar para el aviso en el sitio.</li>
+			<li>Título y texto del aviso, in</li>
+		
+		</ul>
+	</li>
+</ol>
+
+<p>Cada sponsorship permite derecho a una <b>credencial personalizada</b> y a publicar <b>un aviso para búsqueda de un perfil</b>.
+	
+
+<h3>Fabula romana clara est</h3>
+
+<p>Zzril delenit augue duis dolore te feugait nulla. Velit esse molestie consequat vel illum dolore eu feugiat nulla vel eum iriure.</a>
+</p>
+
+<p>Cum soluta nobis eleifend option congue nihil imperdiet doming id. Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi qui nunc! Usus legentis in iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod ii?</p>
+
+<a href="empleos.php">&laquo; reddo a domus</a>
+
+
+
+
+			</div><?php /* /colContent */ ?>
+
+
+<?php include ( "bits/mainbody-coletc-logos.php" ); ?>
+
+
+		</div><?php /* /layoutContentEtc */ ?>
+
+
+</div><?php /* /mbody */ ?>
+
+</div><?php /* /mainbody */ ?>
+				
+
+
+
+
+
+
+
+
+<?php include ( "bits/mainfoot.php" ); ?>
+
+
+</body>
+</html>
--- empleos.php
+++ empleos.php
@@ -7,7 +7,7 @@
 	
 	?><html lang="es">
 
-<head>
+<?php /*<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
@@ -21,8 +21,13 @@
 	<meta content="UX2012: 28/nov/2012 Centro Cultural San Martín, en el marco del Día Mundial de la Usabilidad. El principal evento profesional en Argentina sobre Experiencia de Usuario, Usabilidad, Diseño de Interacción, Accesibilidad y temáticas relacionadas." property="og:description">
 
 	<link href="rsrc/base.css" media="screen" rel="stylesheet" type="text/css">	
-</head>
+</head>*/ ?>
 
+<?php
+
+	$pageTitle = "Empleos";
+	include ( "bits/_htmlhead.php" );
+	?>
 
 <body class="single">
 
@@ -42,7 +47,7 @@
 
 <p>Cum soluta nobis eleifend option congue nihil imperdiet doming id. Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi qui nunc! Usus legentis in iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod ii?</p>
 
-<p>Zzril delenit augue duis dolore te feugait nulla. Velit esse molestie consequat vel illum dolore eu feugiat nulla facilisis. Duis autem vel eum iriure <a href="empleos-publicar.php">vero eros et accumsan &raquo;</a>
+<p>Zzril delenit augue duis dolore te feugait nulla. Velit esse molestie consequat vel illum dolore eu feugiat nulla vel eum iriure <a href="empleos-publicar.php">sumate y public&aacute; tu aviso &raquo;</a>
 </p>
 
 <?php // include ( 'bits/boxcall2atn-publicar.php' ); ?>
