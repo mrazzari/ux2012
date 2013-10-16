5a77a43e40f22269cfbec485f3461b6abf68241d
Jose Allona <jallona@gmail.com>
2013-10-16 10:35:34 -0300

    comento script deprecado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,12 @@
 // custom scripts
 
 jQuery(function($) {
-	
+	/*
 	$('a').click(function(){
 	    $('html, body').animate({
 	        scrollTop: ($( $.attr(this, 'href') ).offset().top) - 50
 	    }, 500);
 	    return false;
 	});
-
+	*/
 });
