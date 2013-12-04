503a327272ae7eaa8d28f50fb8c245286525bb0f
Manuel Razzari <manuel@ultimorender.com.ar>
2013-12-04 11:12:18 -0300

    Cambiada URL encuesta dia 2.

--- encuesta/index.php
+++ encuesta/index.php
@@ -1,4 +1,4 @@
 <?php
 header('HTTP/1.1 302 Moved Temporarily');
-header('Location: https://docs.google.com/forms/d/10OKhzsuOzahMgGWpRXTXfj0uIFzxTQTeKKOuJFx4dDQ/viewform');
+header('Location: https://docs.google.com/forms/d/1fEAie9xX39tWN9NzJH0dgS6RtDWdFJXBZXKwPcAgkPw/viewform');
 die();
